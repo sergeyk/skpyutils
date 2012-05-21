fc7df68616c39424c1988f80d3bfe0439c139f11
Sergey Karayev <sergeykarayev@gmail.com>
2012-05-21 10:30:29 -0700

    added common_imports stuff

---
+++ common_imports.py
@@ -0,0 +1,18 @@
+import os
+from os.path import join as opjoin
+from os.path import exists as opexists
+
+import re
+import operator
+import json
+import itertools
+import pickle,cPickle
+
+from IPython import embed
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import skpyutils.util as ut
+import skpyutils.mpi as mpi
