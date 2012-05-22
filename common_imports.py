f21a51e8ec90a83462267266fc4ec7fadab20015
Sergey Karayev <sergeykarayev@gmail.com>
2012-05-21 22:56:32 -0700

    pandas

--- common_imports.py
+++ common_imports.py
@@ -11,8 +11,11 @@ import pickle,cPickle
 from IPython import embed
 
 import numpy as np
+import sklearn
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+from pandas import DataFrame,Series,Panel
 
 import skpyutils.util as ut
 import skpyutils.common_mpi as mpi
