9ae1cc9571247c2700e6d4310496fa07e16d45de
Sergey Karayev <sergeykarayev@gmail.com>
2012-06-21 02:51:16 -0400

    fixed common_mpi

--- common_mpi.py
+++ common_mpi.py
@@ -1,10 +1,11 @@
 from mpi4py import MPI
+import time
 
 comm = MPI.COMM_WORLD
 comm_rank = comm.Get_rank()
 comm_size = comm.Get_size()
 
-def safebarrier(comm=None, tag=0, sleep=0.01):
+def safebarrier(comm==None, tag=0, sleep=0.01):
   """
   This is a better mpi barrier than MPI.comm.barrier(): the original barrier
   may cause idle processes to still occupy the CPU, while this barrier waits
@@ -12,19 +13,19 @@ def safebarrier(comm=None, tag=0, sleep=0.01):
   
   Code by "Yanging Jia" <jiayq@icsi.berkeley.edu>
   """
-  if comm == None:
+  if comm==None:
     comm = MPI.COMM_WORLD
   size = comm.Get_size()
   if size == 1:
     return
-    rank = comm.Get_rank()
-    mask = 1
-    while mask < size:
-      dst = (rank + mask) % size
-      src = (rank - mask + size) % size
-      req = comm.isend(None, dst, tag)
-      while not comm.Iprobe(src, tag):
-        time.sleep(sleep)
-        comm.recv(None, src, tag)
-        req.Wait()
-        mask <<= 1
+  rank = comm.Get_rank()
+  mask = 1
+  while mask < size:
+    dst = (rank + mask) % size
+    src = (rank - mask + size) % size
+    req = comm.isend(None, dst, tag)
+    while not comm.Iprobe(src, tag):
+      time.sleep(sleep)
+    comm.recv(None, src, tag)
+    req.Wait()
+    mask <<= 1
